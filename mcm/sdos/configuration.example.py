78614491611a751ba29fe77fcc3dc8aa338ff56f
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2017-02-20 15:52:13 +0100

    new default env

--- mcm/sdos/configuration.example.py
+++ mcm/sdos/configuration.example.py
@@ -41,10 +41,11 @@ SDOS runs both on the same host/port; only URL-path differs.
 Keystone auth (p. 5000) __________ SDOS proxy (p. 3000) __________ Swift client
                               /
 Swift store   (p. 8080) _____/
+
+configure the proxy endpoint that clients connect to
+my_endpoint_host gets advertised to clients after AUTH. So this needs to be the address that clients see
 """
 
-# configure the proxy endpoint that clients connect to
-# my_endpoint_host gets advertised to clients after AUTH. So this needs to be the address that clients see
 
 my_endpoint_port = 3000
 my_endpoint_host = os.getenv("MY_ENDPOINT_HOST", "localhost")
@@ -53,26 +54,40 @@ my_bind_host = "0.0.0.0"
 my_endpoint_store_url = "http://{}:{}/v1/AUTH_{}".format(my_endpoint_host, my_endpoint_port, "{}")
 
 
-# configure the swift connection
+"""
+################################################################################
+Swift backend - auth API endpoint
+################################################################################
+"""
 
-# docker CEPH
 swift_auth_host = os.getenv("SWIFT_AUTH_HOST", "localhost")
-swift_auth_port = os.getenv("SWIFT_AUTH_PORT", "8080")
+swift_auth_port = os.getenv("SWIFT_AUTH_PORT", 8080)
 
-# v2 keystone auth
-#swift_auth_url = "http://{}:{}/v2.0/tokens".format(swift_auth_host, swift_auth_port)
-# v1 CEPH auth
-#swift_auth_url = "http://{}:{}/auth/1.0".format(swift_auth_host, swift_auth_port)
+
 # v1 swift auth
 swift_auth_url = "http://{}:{}/auth/v1.0".format(swift_auth_host, swift_auth_port)
 
+# v2 keystone auth
+#swift_auth_url = "http://{}:{}/v2.0/tokens".format(swift_auth_host, swift_auth_port)
+
+# v1 CEPH auth
+#swift_auth_url = "http://{}:{}/auth/1.0".format(swift_auth_host, swift_auth_port)
+
+
+
+"""
+################################################################################
+Swift backend - store API endpoint
+################################################################################
+"""
 
 # docker CEPH
 swift_store_host = os.getenv("SWIFT_STORE_HOST", "localhost")
-swift_store_port = os.getenv("SWIFT_STORE_PORT", "8080")
+swift_store_port = os.getenv("SWIFT_STORE_PORT", 8080)
 #enigma ssh tunnel
 
 # openstack Swift
 swift_store_url = "http://{}:{}/v1/AUTH_{}".format(swift_store_host, swift_store_port, "{}")
+
 # CEPH on port :80
 #swift_store_url = "http://{}/swift/v1".format(swift_store_host)
