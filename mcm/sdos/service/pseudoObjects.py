a8341f17c86ec4242c022ac25687b7f17b077b42
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2017-02-22 22:02:13 +0100

    introduce crypto-only frontend

--- mcm/sdos/service/pseudoObjects.py
+++ mcm/sdos/service/pseudoObjects.py
@@ -18,6 +18,7 @@
 """
 
 import logging
+import json
 from flask import Response
 
 from mcm.sdos.service.Exceptions import HttpError
@@ -39,77 +40,86 @@ def extract_passphrase(msg):
         return None
 
 
-def dispatch_get_head(sdos_frontend, thisObject):
+def dispatch_get_head(frontend, thisObject):
     logging.debug("GET/HEAD request for MCM pseudo object: {}".format(thisObject))
     is_operation = lambda name: (thisObject[len(PSEUDO_OBJECT_PREFIX):] == name)
-    try:
-        cascade = sdos_frontend.cascade
-        ###############################################################################
-        # statistics, visualization
-        ###############################################################################
-        if is_operation("sdos_used_partitions"):
-            return Response(response=treeGeometry.sdos_used_partitions(cascade=cascade), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_partition_mapping"):
-            return Response(response=treeGeometry.sdos_partition_mapping(cascade=cascade), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_batch_delete_log"):
-            return Response(response=treeGeometry.sdos_batch_delete_log(sdos_frontend=sdos_frontend), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_cascade_stats"):
-            return Response(response=treeGeometry.sdos_cascade_stats(sdos_frontend=sdos_frontend), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_slot_utilization10"):
-            return Response(response=treeGeometry.sdos_slot_utilization(cascade=cascade, NUMFIELDS=10), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_slot_utilization100"):
-            return Response(response=treeGeometry.sdos_slot_utilization(cascade=cascade, NUMFIELDS=100), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_slot_utilization1000"):
-            return Response(response=treeGeometry.sdos_slot_utilization(cascade=cascade, NUMFIELDS=1000), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_slot_utilization10000"):
-            return Response(response=treeGeometry.sdos_slot_utilization(cascade=cascade, NUMFIELDS=10000), status=200,
-                            mimetype="application/json")
-        ###############################################################################
-        # unknown
-        ###############################################################################
-        else:
-            raise HttpError("unknown pseudo object: {}".format(thisObject))
-    except Exception as e:
-        logging.exception("pseudo object API received exception")
-        return "{}".format(e), 500
+    ###############################################################################
+    # statistics, visualization
+    ###############################################################################
+    if is_operation("crypto_key_stats"):
+        return Response(response=json.dumps(frontend.key_source.get_status_json()), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_used_partitions"):
+        return Response(response=treeGeometry.sdos_used_partitions(cascade=frontend.cascade), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_partition_mapping"):
+        return Response(response=treeGeometry.sdos_partition_mapping(cascade=frontend.cascade), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_batch_delete_log"):
+        return Response(response=treeGeometry.sdos_batch_delete_log(sdos_frontend=frontend), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_cascade_stats"):
+        return Response(response=treeGeometry.sdos_cascade_stats(sdos_frontend=frontend), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_slot_utilization10"):
+        return Response(response=treeGeometry.sdos_slot_utilization(cascade=frontend.cascade, NUMFIELDS=10), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_slot_utilization100"):
+        return Response(response=treeGeometry.sdos_slot_utilization(cascade=frontend.cascade, NUMFIELDS=100),
+                        status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_slot_utilization1000"):
+        return Response(response=treeGeometry.sdos_slot_utilization(cascade=frontend.cascade, NUMFIELDS=1000),
+                        status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_slot_utilization10000"):
+        return Response(response=treeGeometry.sdos_slot_utilization(cascade=frontend.cascade, NUMFIELDS=10000),
+                        status=200,
+                        mimetype="application/json")
+    ###############################################################################
+    # unknown
+    ###############################################################################
+    else:
+        raise HttpError("unknown pseudo object: {}".format(thisObject))
 
 
-def dispatch_put_post(sdos_frontend, thisObject, data):
+def dispatch_put_post(frontend, thisObject, data):
     logging.debug("PUT/POST request for MCM pseudo object: {}, data: {}".format(thisObject, data))
     is_operation = lambda name: (thisObject[len(PSEUDO_OBJECT_PREFIX):] == name)
+    p = extract_passphrase(data)
+
     try:
-        cascade = sdos_frontend.cascade
-        p = extract_passphrase(data)
-        ###############################################################################
-        # key management actions
-        ###############################################################################
-        if is_operation("sdos_next_deletable"):
-            return Response(response=cascade.masterKeySource.provide_next_deletable(passphrase=p), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_clear_next_deletable"):
-            return Response(response=cascade.masterKeySource.clear_next_deletable(), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_masterkey_unlock"):
-            return Response(response=cascade.masterKeySource.unlock_key(passphrase=p), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_masterkey_lock"):
-            return Response(response=cascade.masterKeySource.lock_key(), status=200,
-                            mimetype="application/json")
-        elif is_operation("sdos_batch_delete_start"):
-            return Response(response=sdos_frontend.batch_delete_start(), status=200,
-                            mimetype="application/json")
-        ###############################################################################
-        # unknown
-        ###############################################################################
-        else:
-            raise HttpError("unknown pseudo object: {}".format(thisObject))
-    except Exception as e:
-        logging.exception("pseudo object API received exception")
-        return "{}".format(e), 500
+        frontend.cascade  # sdos frontend
+        key_source = frontend.cascade.masterKeySource
+    except AttributeError:
+        pass
+    try:
+        frontend.key_source  # sdos frontend
+        key_source = frontend.key_source
+    except AttributeError:
+        pass
+
+
+    ###############################################################################
+    # key management actions
+    ###############################################################################
+    if is_operation("sdos_next_deletable"):
+        return Response(response=key_source.provide_next_deletable(passphrase=p), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_clear_next_deletable"):
+        return Response(response=key_source.clear_next_deletable(), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_masterkey_unlock"):
+        return Response(response=key_source.unlock_key(passphrase=p), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_masterkey_lock"):
+        return Response(response=key_source.lock_key(), status=200,
+                        mimetype="application/json")
+    elif is_operation("sdos_batch_delete_start"):
+        return Response(response=frontend.batch_delete_start(), status=200,
+                        mimetype="application/json")
+    ###############################################################################
+    # unknown
+    ###############################################################################
+    else:
+        raise HttpError("unknown pseudo object: {}".format(thisObject))
