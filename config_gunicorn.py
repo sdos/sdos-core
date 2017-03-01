f22fd13340deab2fe08640a9594a6cc5be5ccdf3
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2017-03-01 09:44:35 +0100

    move to gunicorn server

--- config_gunicorn.py
+++ config_gunicorn.py
@@ -12,13 +12,14 @@
 from mcm.sdos import configuration
 import multiprocessing
 
-
 bind = "{}:{}".format(configuration.my_bind_host, configuration.my_endpoint_port)
-workers = multiprocessing.cpu_count() * 2 + 1
-#workers = workers * 1
+# workers = multiprocessing.cpu_count() * 2 + 1
+# workers = workers * 1
+# gunicorn runs multi-process; this currently does not work with the Key Cascade due to the need for locking
+# which we can only do in a multi-thread, but no multi-process scenario
 workers = 1
 timeout = 600
 graceful_timeout = 800
 worker_class = "gevent"
-#worker_class = "eventlet"
-#worker_class = "geventwebsocket.gunicorn.workers.GeventWebSocketWorker"
+# worker_class = "eventlet"
+# worker_class = "geventwebsocket.gunicorn.workers.GeventWebSocketWorker"
